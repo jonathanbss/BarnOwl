c2a8ea406a70e0c31f70a5c939d276273285eaed
start.vaadin.com <no-reply@vaadin.com>
2024-06-17 07:25:12 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/home/HomeView.java
@@ -0,0 +1,33 @@
+package com.example.application.views.home;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import jakarta.annotation.security.PermitAll;
+
+@PageTitle("Home")
+@Menu(icon = "line-awesome/svg/pencil-ruler-solid.svg", order = 0)
+@Route(value = "")
+@RouteAlias(value = "")
+@PermitAll
+public class HomeView extends Composite<VerticalLayout> {
+
+    public HomeView() {
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.setHeight("min-content");
+        layoutColumn2.setWidth("100%");
+        layoutColumn2.getStyle().set("flex-grow", "1");
+        getContent().add(layoutRow);
+        getContent().add(layoutColumn2);
+    }
+}
